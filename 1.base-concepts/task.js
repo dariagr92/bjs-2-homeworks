b35db452c958930249805ce5d4be1397cef53be5
Дарья Грачева <daherishka@yandex.ru>
2022-08-10 12:34:37 +0500

    1 Task solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,26 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
+   let arr;
   // код для задачи №1 писать здесь
+  let d, x1, x2;
+  
+  d = b**2 - 4*a*c;
+  if (d < 0) {
+    arr = [];
+  }
+  else if (d == 0){
+    x1 = - b/(2*a);
+    arr = [x1];
+  }
+  else{
+    x1 = (-b + Math.sqrt(d))/(2*a);
+    x2 = (-b - Math.sqrt(d))/(2*a);
+    arr = [x1, x2];
+  } 
   return arr; // array
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
@@ -11,3 +29,4 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
   return totalAmount;
 }
+
