72a145c047a126d7b4e271675119bd4245ec2b0a
Дарья Грачева <daherishka@yandex.ru>
2022-08-11 16:23:38 +0500

    2 task corrected

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -20,33 +20,38 @@ function solveEquation(a, b, c) {
 
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount, percentCh, contributionCh, amountCh, dateCh;
+  let totalAmount, percentCh, contributionCh, amountCh, dateCh, message;
+
   percentCh = Number(percent);
   contributionCh = Number(contribution);
   amountCh = Number(amount);
   dateCh = Number(date);
 
-  if (typeof percentCh !== 'Number'){
-        console.log(`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`); 
+  if (isNaN(percentCh)){
+    message = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`
+    return message; 
   }
-  if (typeof contributionCh !== 'Number'){
-    console.log(`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`); 
+  if (isNaN(contribution)){
+    message = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+    return message; 
   }
-  if (typeof amountCh !== 'Number'){
-    console.log(`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`); 
+  if (isNaN(amountCh)){
+    message = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`; 
+    return message; 
   }
-  if (typeof dateCh !== 'Number'){
-    console.log(`Параметр "Дата" содержит неправильное значение "${date}"`); 
+  if (isNaN(dateCh)){
+    message; `Параметр "Дата" содержит неправильное значение "${date}"`;
+    return message;  
   }
 
-  let dateNow = Date.now;
+  let dateNow = Date.now();
   let term, creditBody, P, payment, totalSumm;
-  term = (dateCh - dateNow())/1000/60/60/24/30;
+  term = Math.floor((dateCh - dateNow)/1000/60/60/24/30);
   creditBody = amountCh - contributionCh;
   P = percentCh / 1200;
   payment = creditBody * (P + (P / (((1 + P)**term) - 1)));
   totalSumm = payment * term;
-  totalAmount = totalSumm.toFixed(2);
+  totalAmount = Number(totalSumm.toFixed(2));
  
 
   // код для задачи №2 писать здесь
