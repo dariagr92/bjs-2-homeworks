781d3db217cc42cc8a519e1e803895892ed1d391
Дарья Грачева <daherishka@yandex.ru>
2022-08-11 08:44:14 +0500

    1 task corrected

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,21 +1,18 @@
 'use strict';
 
 function solveEquation(a, b, c) {
-   let arr;
+   let arr = [];
   // код для задачи №1 писать здесь
-  let d, x1, x2;
-  
+  let d;
   d = b**2 - 4*a*c;
-  if (d < 0) {
-    arr = [];
-  }
-  else if (d == 0){
-    x1 = - b/(2*a);
+ 
+  if (d == 0){
+    let x1 = - b/(2*a);
     arr = [x1];
   }
-  else{
-    x1 = (-b + Math.sqrt(d))/(2*a);
-    x2 = (-b - Math.sqrt(d))/(2*a);
+  else if (d > 0){
+    let x1 = (-b + Math.sqrt(d))/(2*a);
+    let x2 = (-b - Math.sqrt(d))/(2*a);
     arr = [x1, x2];
   } 
   return arr; // array
