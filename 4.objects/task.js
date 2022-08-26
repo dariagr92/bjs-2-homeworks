bcfb3e5f4cf87eab2f70a80126a319273788f179
Дарья Грачева <daherishka@yandex.ru>
2022-08-26 13:49:47 +0500

    Task is done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+this.name = name;
+this.gender = gender;
+this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
   //ваш код
 }
+
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+  this.marks = [mark]; 
+} else {this.marks.push(mark);}
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined){ 
+    this.marks = [...marks]; }
+    else {this.marks.push(...marks);}
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i++){
+    sum += this.marks[i];
+  }
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason){
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
 
 // ваш код для остальных методов
