a32db8e5fffb0dccc23ff2fa9b1d64cc39953d62
Дарья Грачева <daherishka@yandex.ru>
2022-08-26 09:06:20 +0500

    1 task corrected

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
- 
-  return arr1.length == arr2.length && arr1.every((value, index) => value === arr2[index]); // boolean
+  let result;
 
+   result = arr1.length == arr2.length && arr1.every((value, index) => value === arr2[index]); 
+
+   return result; // boolean
 }
 
 
