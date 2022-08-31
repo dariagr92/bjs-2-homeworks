572e6715d36a12ffff647d54a48e40e6295d60d2
Дарья Грачева <daherishka@yandex.ru>
2022-08-31 21:56:15 +0500

    task in progress

--- 7.async/task.js
+++ 7.async/task.js
@@ -27,8 +27,51 @@ class AlarmClock {
     removeClock(id) {
             const removedId = this.alarmCollection.filter(item => item.id === id)
             
-
+        if (removedId){
+            this.alarmCollection.pop();
+        }
+        return this.alarmCollection.length != this.alarmCollection.length;
             
     }
+
+    getCurrentFormattedTime(time){
+        const currentDate = new Date();
+        const hours = currentDate.getHours() < 10 ? `0${currentDate.getHours()}` : `${currentDate.getHours()}`;
+        const minutes = currentDate.getMinutes() < 10 ? `0${currentDate.getMinutes()}` : `${currentDate.getMinutes()}`;
+        return `${hours}:${minutes}`;
+    }
+
+    start(){
+        const checkClock = call =>{
+            if (this.alarmCollection.time === this.getCurrentFormattedTime()){
+                this.alarmCollection.callback();
+            }
+
+            if (this.timerId === null){
+                this.timerId = setInterval(() => this.alarmCollection.forEach(checkClock),this.timerId);
+            }
+        }
+    }
+
+    stop(){
+        if (this.timerId != null){
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
+    }
+
+    printAlarms(){
+        this.alarmCollection.forEach(alarm => console.log('Будильник ' + `${alarm.id}` + 'заведен на время: ' + `${alarm.time}`));
+    }    
+    
+    clearAlarms(){
+        clearInterval(this.timerId);
+        this.alarmCollection = [];
+        this.timerId = null;
+    }
+}
+
+function testCase(){
+    let alarm1 = new AlarmClock();
     
 }
