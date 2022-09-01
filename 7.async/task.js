bff5ece9aadb6eea5796ca24d471d44ba01edc94
Дарья Грачева <daherishka@yandex.ru>
2022-09-01 10:16:26 +0500

    task is done

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,27 +1,23 @@
 
 
 class AlarmClock {
-    constructor (alarmCollection = [], timerId = null){
-        this.alarmCollection = alarmCollection;
-        this.timerId = timerId;
+    constructor (alarmCollection, timerId){
+        this.alarmCollection = [];
+        this.timerId = null;
     }
 
-    addClock(time, callback, id){
-        if (!id){
-            throw new Error ('Параметр не передан!');
+    addClock(time, callback, id) {
+        if (id === undefined) {
+            throw new Error ('Параметр ID не передан!');
         }
 
         else if (this.alarmCollection.find(item => item.id === id)){
-            console.error('Такой звонок уже существует');
+           return console.error('Такой звонок уже существует');
         }
 
-        else {
-            this.alarmCollection.push({
-                time: time,
-                callback: callback,
-                id: id
-            })
-        }
+        this.alarmCollection.push({time, callback, id});
+        
+     
     }
 
     removeClock(id) {
@@ -61,7 +57,7 @@ class AlarmClock {
     }
 
     printAlarms(){
-        this.alarmCollection.forEach(alarm => console.log('Будильник ' + `${alarm.id}` + 'заведен на время: ' + `${alarm.time}`));
+        this.alarmCollection.forEach(alarm => console.log('Будильник ' + `${alarm.id}` + ' заведен на время: ' + `${alarm.time}`));
     }    
     
     clearAlarms(){
@@ -71,7 +67,36 @@ class AlarmClock {
     }
 }
 
-function testCase(){
-    let alarm1 = new AlarmClock();
+let clock = new AlarmClock();
+
+console.log('Первый будильник ' + clock.addClock("07:00", () => console.log('Вставай!'), 1));
+console.log('Количество записей - ' + clock.alarmCollection.length);
+
+clock.removeClock(1);
+console.log('Теперь количество записей - ' + clock.alarmCollection.length);
+
+const currentDate = new Date();
+console.log('Показываем текущее время - ' + clock.getCurrentFormattedTime());
+
+console.log('id интервала до запуска - ' + clock.timerId);
+
+console.log('Второй будильник ' + clock.addClock("07:00", () => console.log('Вставай уже!'), 2));
+
+console.log('Запускаем ' + clock.start());
+console.log(clock.timerId);
+
+console.log('Третий будильник ' + clock.addClock("07:05", () => console.log('Вставай уже!'), 3));
+console.log('Четвертый будильник ' + clock.addClock("07:10", () => console.log('Вставаааай!'), 4));
+console.log('Пятый будильник ' + clock.addClock("07:15", () => console.log('Щас проспишь!'), 5));
+
+console.log('Сейчас записей - ' + clock.alarmCollection.length);
+clock.printAlarms();
+
+
+clock.clearAlarms();
+console.log('После удаления записей - ' + clock.alarmCollection.length);
+
+
+
+
     
-}
